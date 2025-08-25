bbe80763330d9e00c859519ea38f07e2a2bf2f63
Alexis John Alcoriza <alexisalcoriza1@gmail.com>
2025-08-25 18:45:27 +0800

    test: added CART-001&2 scripts

---
+++ tests/scripts/Cart/CART-002.spec.js
@@ -0,0 +1,57 @@
+import { test, expect } from '@playwright/test'
+import { LoginPage} from '../../../pages/login.page'
+import { ProductsPage} from '../../../pages/products.page'
+import {CartPage} from '../../../pages/cart.page'
+
+
+test.describe('Product Cart Funtionality', () =>{
+
+    let loginPage
+    let productsPage
+    let cartPage
+    //Define the product name you want to add to the cart
+    const productName = 'Sauce Labs Backpack'; // You can replace this with any valid product name on the site
+
+    test.beforeEach(async({page}) =>{
+    
+            //Pre-conditions: The user is logged in and on the product page
+            //Initialize Login page and Products page
+            loginPage = new LoginPage(page)
+            productsPage = new ProductsPage(page)
+    
+             //go to login page url
+             await loginPage.gotoLoginPage();
+        
+             //input username and password then login
+             await loginPage.enterUsername('standard_user')
+             await loginPage.enterPassword('secret_sauce')
+             await loginPage.clickLogin()
+         
+             //Asssertion: Verify if the user is on the product page
+             await loginPage.pageLanding()
+    
+    
+        })
+        test('Verify if "Continue shopping" button is functional.',async({page})=>{
+
+          
+
+            //Initialize Cart Page
+            cartPage = new CartPage(page)
+
+            //Step 3: Go to Cart Page
+            cartPage.cartPageLanding()
+
+            //Step 4: Verify if Continue shipping button is functional.
+            await page.getByRole('link', { name: 'Continue Shopping'}).click();
+
+            //Step 5: Veriofy if the user is navigated back to the product page
+            await Login.pageLanding()
+
+
+
+        })
+        
+})
+
+
